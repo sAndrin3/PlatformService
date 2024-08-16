77737adb3ad7f99cbb080475f70d2cf7c51cebdf
sAndrin3 <sandrinemalanga2001@gmail.com>
2024-08-16 21:00:26 +0300

    ingress-nginx

--- CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
+++ CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16edfb87fe9fd2832a165dbb21cbb43d393dd3d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
